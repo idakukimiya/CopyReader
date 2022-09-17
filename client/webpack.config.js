a3f8dd71d1674765c888a3d46efd3d2b734bf084
idakukimiya <kukimiya.ida@gmail.com>
2022-09-16 23:57:24 -0400

    update all package.json

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,7 +18,32 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+				template: './index.html',
+			}),
+			new WebpackPwaManifest({
+				name: 'COPYREADER',
+				short_name: 'COPYREADER',
+				description: 'copyreader',
+				display: 'standalone',
+				background_color: '#1f2554',
+				theme_color: '#1f2554',
+				start_url: '/',
+				publicPath: '/',
+				fingerprints: false,
+				inject: true,
+				icons: [
+					{
+						src: path.resolve('src/images/logo.png'),
+						sizes: [96, 128, 192, 256, 384, 512],
+						destination: path.join('assets', 'icons'),
+					},
+				],
+			}),
+			new InjectManifest({
+				swSrc: './src-sw.js',
+				swDest: 'service-worker.js',
+			}),
     ],
 
     module: {
